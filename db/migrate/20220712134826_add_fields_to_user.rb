589cfd9b6d0a74ddde830c961351928f0671ffa3
negres <nataliaknegres@gmail.com>
2022-07-12 11:15:31 -0300

    chore: add new fields in the users table

---
+++ db/migrate/20220712134826_add_fields_to_user.rb
@@ -0,0 +1,7 @@
+class AddFieldsToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :cpf, :string
+    add_column :users, :phone, :string
+  end
+end
